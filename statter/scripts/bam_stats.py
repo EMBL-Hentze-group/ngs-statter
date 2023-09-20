5b4dc789e81af97030d5dae841a23a11bd10473f
sahadeva <sudeep.sahadevan@embl.de>
2023-09-20 13:48:08 +0200

    add gene type read length plot cli

--- statter/scripts/bam_stats.py
+++ statter/scripts/bam_stats.py
@@ -2,8 +2,9 @@ import click
 from typing import List
 from parsers.bam_parser import BamParser
 from parsers.gff_parser import GFF3
-CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+from plotters.gene_type_read_length_distribution import GeneTypePlot
 
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
@@ -41,7 +42,7 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
     required=True,
     help="Output file to write json formatted data",
 )
-def gene_read_len_stats(
+def gene_type_read_length_stats(
     gff3: str,
     bam: str,
     out_json: str,
@@ -57,3 +58,26 @@ def gene_read_len_stats(
     gff3_genes = gff3_parse.parse_gene_info()
     bam_parser = BamParser(bam=bam, min_q=min_q)
     bam_parser.read_length_stats_per_gene_type(genes=gff3_genes, out_json=out_json)
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    "--json_dir", "json_dir", required=True, help="Directory containing json formatted read_length files (see read_length_parser -h)"
+)
+@click.option("--pattern","pattern", default = "*.json", help="File naming pattern", show_default = True, type = str)
+@click.option(
+    "--nsamples",
+    "nsamples",
+    default=1,
+    help="Number of replicates per group",
+    show_default=True,
+    type=int,
+)
+@click.option(
+    "--html", "output_html", required=True, help="Plot output file name"
+)
+def gene_type_length_plotter(json_dir: str, output_html:str, pattern:str, nsamples = 1) -> None:
+    '''
+    Read in json formatted gene type read length stats and output html plot
+    '''
+    gtl = GeneTypePlot(json_folder=json_dir, output_file=output_html,pattern=pattern, nsamples=nsamples)
+    gtl.plot()
