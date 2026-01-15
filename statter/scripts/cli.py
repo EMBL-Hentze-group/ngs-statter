b54caa947e6124983165a92103ceeff20649d8c8
sahadeva <sudeep.sahadevan@embl.de>
2026-01-15 09:00:55 +0100

    use simple design

--- statter/scripts/cli.py
+++ statter/scripts/cli.py
@@ -18,3 +18,20 @@ clis = click.CommandCollection(
 
 def main() -> None:
     clis()
+
+
+# if groups need to be nested and visible in help output, use the following pattern:
+# @click.group(context_settings=CONTEXT_SETTINGS)
+# def entry() -> None:
+#     """
+#     run as: statter <group> <command> [argument] [option]
+#     """
+#     pass
+
+
+# entry.add_command(flexbar)
+# entry.add_command(fastq)
+# entry.add_command(alignment)
+# entry.add_command(genetype)
+# entry.add_command(kraken)
+# entry.add_command(sample)
